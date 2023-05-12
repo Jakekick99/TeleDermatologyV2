51de0d96c0d976ba68ec7c6de317185f3ab5e3ee
Jakekick99 <jakekallarackal@gmail.com>
2023-05-12 23:51:56 +0530

    Doctor Client to Server JWT Auth
    > Doctor will now need to login or register to access
    > JWT implemented to handle token based authentication and authorisation

--- admin/src/main/java/com/teledermatology/admin/security/service/AuthenticationService.java
+++ admin/src/main/java/com/teledermatology/admin/security/service/AuthenticationService.java
@@ -1,11 +1,11 @@
-package com.teledermatology.patient.security.service;
+package com.teledermatology.admin.security.service;
 
-import com.teledermatology.patient.bean.entity.PatientEntity;
-import com.teledermatology.patient.bean.entity.Role;
-import com.teledermatology.patient.repository.PatientRepository;
-import com.teledermatology.patient.security.model.AuthenticationRequest;
-import com.teledermatology.patient.security.model.AuthenticationResponse;
-import com.teledermatology.patient.security.model.RegisterRequest;
+import com.teledermatology.admin.bean.entity.DoctorEntity;
+import com.teledermatology.admin.bean.entity.Role;
+import com.teledermatology.admin.repository.DoctorRepository;
+import com.teledermatology.admin.security.model.AuthenticationRequest;
+import com.teledermatology.admin.security.model.AuthenticationResponse;
+import com.teledermatology.admin.security.model.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,24 +15,22 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
-
-    private final PatientRepository patientRepository;
+    private final DoctorRepository patientRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest registerRequest) {
-        var patient = PatientEntity.builder()
-                .aadhaar(registerRequest.getAadhaar())
-                .pid(registerRequest.getPid())
+        var doctor = DoctorEntity.builder()
+                .did(registerRequest.getDid())
                 .fname(registerRequest.getFname())
                 .lname(registerRequest.getLname())
                 .email(registerRequest.getEmail())
                 .pass(passwordEncoder.encode(registerRequest.getPass()))
                 .role(Role.USER)
                 .build();
-        patientRepository.save(patient);
-        String jwt_token = jwtService.generateToken(patient);
+        patientRepository.save(doctor);
+        String jwt_token = jwtService.generateToken(doctor);
         return AuthenticationResponse.builder()
                 .token(jwt_token)
                 .build();
@@ -50,8 +48,8 @@ public class AuthenticationService {
         catch(Exception e){
             System.out.println(e);
         }
-        PatientEntity patient = patientRepository.findByEmail(authenticationRequest.getEmail()).orElseThrow();
-        String jwt_token = jwtService.generateToken(patient);
+        DoctorEntity doctor = patientRepository.findByEmail(authenticationRequest.getEmail()).orElseThrow();
+        String jwt_token = jwtService.generateToken(doctor);
         return AuthenticationResponse.builder()
                 .token(jwt_token)
                 .build();
